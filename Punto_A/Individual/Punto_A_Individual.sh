d5d22daef16542bb443c950a4565e636db87ebdc
Axel Cabral <axelfernandocabral@gmail.com>
2026-06-21 02:28:40 +0000

    Segundo commit con arreglos en el punto A

---
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Script para crear la estructura de directorios del rol sysadmin
+
+mkdir -p ~/sysadmin/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep ~/sysadmin/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
