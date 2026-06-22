4bb983a69d37beb1e167dcca930aa309a3097842
Fernando Cabral <axelcabral94@gmail.com>
2026-06-22 14:41:48 +0000

    Agregando punto A individual DEV

--- Punto_A/Individual/Punto_A_Individual.sh
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -3,3 +3,7 @@
 # Script para crear la estructura de directorios del rol sysadmin
 
 mkdir -p ~/sysadmin/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep ~/sysadmin/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
+
+# Script para crear la estructura de directorios del rol dev
+
+mkdir -p ~/dev/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep ~/dev/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
