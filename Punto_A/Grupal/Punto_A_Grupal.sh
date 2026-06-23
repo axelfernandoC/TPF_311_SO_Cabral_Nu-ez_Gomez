aab8d5b9e2f173417c12c0cf7f884540fd9dde40
Axel Cabral <axelfernandocabral@gmail.com>
2026-06-23 21:26:29 +0000

    Agregando parte grupal punto A

---
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script para generar la estructura grupal de continentes del punto A
+
+# Directorios para generar la estructura de Ameria, Rol = sysadmin
+mkdir -p ~/Continentes/America/Peru/{Lima,Cusco,Arequipa} ~/Continentes/America/Colombia/{Bogotá,Medellin} ~/Continentes/America/Mexico/CiudadDeMexico
+
+# Directorios para generar la estructura de Europa, Rol = manager
+
+mkdir -p ~/Continentes/Europa/España/{Madrid,Barcelona,Sevilla} ~/Continentes/Europa/SegundoFrancia/{Paris,Lyon} ~/Continentes/Europa/Italia/Roma
+
+# Directorios para generar la estructura de Asia, Rol = dev
+
+mkdir -p ~/Continentes/Asia/Japon/{Tokio,Osaka,Kioto} ~/Continentes/Asia/China/{Pekín,Shangái} ~/Continentes/Asia/India/Mumbai
+
+
--- Punto_A/Individual/Punto_A_Individual.sh
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -4,6 +4,10 @@
 
 mkdir -p ~/sysadmin/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep ~/sysadmin/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
 
+# Script para crear la estructura de directorios del rol manager
+
+mkdir -p ~/manager/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep ~/manager/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
+
 # Script para crear la estructura de directorios del rol dev
 
 mkdir -p ~/dev/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep ~/dev/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
